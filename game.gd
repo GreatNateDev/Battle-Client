2d17eac41fc205fba33f3393a0f12909cf78a40e
GreatNateDev <Nate@pelmel.net>
2025-09-02 18:53:54 -0500

    fix

--- game.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-
-func Fullscreen_toggled(toggled_on: bool) -> void:
-	if toggled_on == true:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
-		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
